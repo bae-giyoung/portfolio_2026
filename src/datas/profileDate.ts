7f40d818dcce492e7999ba87d408aa94669ac155
bae-giyoung <giyoung1012@gmail.com>
2026-04-21 21:52:45 +0900

    chore: 메인 페이지용 이미지 에셋 및 목업 데이터 추가
    - 프로필 및 프로젝트 관련 이미지(.jpg, .webp) 리소스 추가 및 디렉토리 정리
    - 화면 렌더링을 위한 데이터 구조(profileDate.ts, workData.ts) 추가

---
+++ src/datas/profileDate.ts
@@ -0,0 +1,46 @@
+import { type CarouselSlide } from "@/components/ui/CarouselSlider";
+import profile01 from "@/assets/profile_01.webp";
+import profile02 from "@/assets/profile_02.webp";
+import profile03 from "@/assets/profile_03.webp";
+
+export const profileSlides: CarouselSlide[] = [
+    { src: profile01, alt: "프로필 사진 1", title: "FrontEnd Developer" },
+    { src: profile02, alt: "프로필 사진 2", title: "Coffee Addict" },
+    { src: profile03, alt: "프로필 사진 3", title: "Healing Mode" },
+];
+
+export const skillSet = [
+    "Javascript",
+    "Typescript",
+    "React",
+    "Next.js",
+    "Java",
+    "Spring Boot",
+    "MySQL",
+    "Python",
+];
+
+export const relevantExperience = [
+    { title: "주식회사 아리모아 웹 퍼블리셔 실무", period: "2023.11. - 2025.04." },
+    { title: "부산대학교 주관 『AI 데이터 분석 풀스택 웹 개발자 양성과정』 수료", period: "2025.05. - 2025.11." },
+    { title: "풀스택 역량 개발 중", period: "2026.02. - 현재" }
+];
+
+export const allExperience = [
+    { title: "부산대학교", desc: "대학교(학사) | 의류학과", period: "2009.03. - 2014.02." },
+    { title: "삼성물산 신입 패션전문직 공채과정 인턴", period: "2015.01. - 2015.01." },
+    { title: "(주)씨엔엘 의류디자이너", desc: "대리 | 6년차", period: "2017.02. - 2022.12." },
+    { title: "주식회사 아리모아 웹 퍼블리셔", desc: "주임 | 2년차", period: "2023.11. - 2025.04." },
+    { title: "부산대학교 주관 『AI 데이터 분석 풀스택 웹 개발자 양성과정』 수료", period: "2025.05. - 2025.11." },
+];
+
+export const education = [
+    { title: "부산대학교", desc: "대학교(학사) | 의류학과", period: "2009.03. - 2014.02." },
+    { title: "부산대학교 주관 『AI 데이터 분석 풀스택 웹 개발자 양성과정』 수료", period: "2025.05. - 2025.11." }
+];
+
+export const workExperience = [
+    { title: "주식회사 아리모아 웹 퍼블리셔", desc: "주임 | 2년차", period: "2023.11. - 2025.04." },
+    { title: "(주)씨엔엘 의류디자이너", desc: "대리 | 6년차", period: "2017.02. - 2022.12." },
+    { title: "삼성물산 신입 패션전문직 공채과정 인턴", period: "2015.01. - 2015.01." }
+];
---
+++ src/datas/workData.ts
@@ -0,0 +1,38 @@
+import { type StaticImageData } from "next/image";
+import gallImg01 from "@/assets/gall_1.webp";
+import gallImg02 from "@/assets/gall_2.webp";
+import gallImg03 from "@/assets/gall_3_2.webp";
+import gallImg04 from "@/assets/gall_4.webp";
+import gallImg05 from "@/assets/branding.webp";
+import gallImg06 from "@/assets/neopharm.webp";
+import gallImg07 from "@/assets/adererror_1.webp";
+
+export const WorkGalleryItems01: { src: StaticImageData; alt: string; className: string }[] = [
+    { src: gallImg01, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg02, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg03, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg04, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg05, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg06, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg07, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+];
+
+export const WorkGalleryItems02: { src: StaticImageData; alt: string; className: string }[] = [
+    { src: gallImg01, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg02, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg03, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg04, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg05, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg06, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg07, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+];
+
+export const GalleryItems: { src: StaticImageData; alt: string; className: string }[] = [
+    { src: gallImg01, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg02, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg03, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg04, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg05, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg06, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+    { src: gallImg07, alt: "profile image", className: "relative w-100 h-100 shrink-0" },
+];
