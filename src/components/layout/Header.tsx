e51e4bd23271a234d0e0451079019da7f869247c
bae-giyoung <giyoung1012@gmail.com>
2026-04-27 22:21:34 +0900

    style: Header 레이아웃 미세 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -22,37 +22,35 @@ export default function Header() {
 		}
 	}, [pathname]);
 
-  return (
-	<>
-		<div id="header-wrap" className="fixed w-full h-(--header-height) px-5 md:px-7.5 lg:px-18 py-3 font-inst text-app-fg z-10">
-			{/* 모바일: 사이드 네비게이션 */}
-			<SideNav menuList={[...menuList, ...subMenuList]} linkList={externalLinkList} />
+	return (
+		<>
+			<div id="header-wrap" className="fixed w-full h-(--header-height) px-5 md:px-7.5 lg:px-18 py-3 font-inst text-app-fg z-10">
+				{/* 모바일: 사이드 네비게이션 */}
+				<SideNav menuList={[...menuList, ...subMenuList]} linkList={externalLinkList} />
 
-			{/* 모바일: 메뉴 토글 버튼 */}
-			<MenuButton className="block lg:hidden absolute right-5 md:right-7.5 top-4.5 sm:5.75 text-[16px] in-[.menu-open]:text-app-bg sm:text-lg font-medium transition-colors duration-300 z-30 cursor-pointer" />
-			
-			{/* 헤더 */}
-			<header id="header" className="relative w-full h-full flex justify-center text-lg">
-				<div id="inner-header" className="w-full max-w-441.5 flex items-start justify-between pt-2.5">
+				{/* 모바일: 메뉴 토글 버튼 */}
+				<MenuButton className="block lg:hidden absolute right-5 md:right-7.5 top-4.5 sm:5.75 text-[16px] in-[.menu-open]:text-app-bg sm:text-lg font-medium transition-colors duration-300 z-30 cursor-pointer" />
+				
+				{/* 헤더 */}
+				<header id="header" className="relative w-full h-full flex justify-center text-lg">
+					<div id="inner-header" className="w-full max-w-441.5 flex items-start justify-between pt-2.5">
 
-					{/* 로고 */}
-					<Logo />
+						{/* 로고 */}
+						<Logo />
 
-					<div className="flex gap-6 sm:gap-8 items-center justify-between pr-20 sm:pr-22 lg:pr-0 shrink-0">
-						{/* 외부 링크: GitHub, Notion */}
-						<LinkList linkList={externalLinkList} listClassName="hidden lg:flex gap-6 sm:gap-8 items-center justify-between shrink-0" />
-						{/* 테마 토글 버튼 */}
-						<ThemeToggleButton />
-					</div>
+						<div className="flex gap-6 sm:gap-8 items-center justify-between pr-20 sm:pr-22 lg:pr-0 shrink-0">
+							{/* 외부 링크: GitHub, Notion */}
+							<LinkList linkList={externalLinkList} listClassName="hidden lg:flex gap-6 sm:gap-8 items-center justify-between shrink-0" />
+							{/* 테마 토글 버튼 */}
+							<ThemeToggleButton />
+						</div>
 
-					{/* 내비게이션 메뉴 */}
-					<div className="flex gap-6 sm:gap-8 items-center justify-between pr-20 sm:pr-22 lg:pr-0 shrink-0">
+						{/* 내비게이션 메뉴 */}
 						<Nav menuList={[...menuList, ...subMenuList]} />
 					</div>
-				</div>
-			</header>
-		</div>
-		<ScrolledHeader />
-	</>
-  );
+				</header>
+			</div>
+			<ScrolledHeader />
+		</>
+	);
 }
